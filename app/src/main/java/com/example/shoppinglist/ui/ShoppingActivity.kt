a3a9be0e2b21077660f7e0eaf553017298c5ccc5
paulpriyam <ppaul2204!gmail.com>
2022-08-10 18:51:33 +0530

    without dependency injection.

--- app/src/main/java/com/example/shoppinglist/ui/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/ShoppingActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
 import com.example.shoppinglist.R
+import com.example.shoppinglist.data.local.ShoppingDatabase
+import com.example.shoppinglist.repository.ShoppingRepository
 
 class ShoppingActivity : AppCompatActivity() {
 
@@ -11,5 +13,9 @@ class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+        val db = ShoppingDatabase(this)
+        val repository = ShoppingRepository(db)
+        val factory = ShoppingViewModelFactory(repository)
+        viewModel = ViewModelProviders.of(this, factory)[ShoppingViewModel::class.java]
     }
 }
